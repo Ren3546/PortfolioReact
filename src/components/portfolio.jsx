25af48f860066e30caf3e102c17f285dc09d5636
Ren3546 <128203284+Ren3546@users.noreply.github.com>
2024-02-08 21:39:46 -0500

    bug fix try 2

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Projects from './projects';
 import password from '../assets/password1.png';
 import weather from '../assets/weather.png';
-import note from '../assets/note.png';
+import noteApp from '../assets/noteApp.jpg';
 import dog from '../assets/dog.png';
 import employee from '../assets/employee.png';
 import social from '../assets/social.png';
@@ -12,7 +12,7 @@ export default function Portfolio() {
   const projects = [
     { title: 'Password Generator', imageUrl: password, link: 'https://ren3546.github.io/PasswordGenerator/', githubRepo: 'https://github.com/Ren3546/PasswordGenerator' },
     { title: 'Weather App', imageUrl: weather, link: 'https://ren3546.github.io/Weather-app/', githubRepo: 'https://github.com/Ren3546/Weather-app' },
-    { title: 'Note Taker', imageUrl: note, link: 'https://note-app-rv-b9e70abbcc56.herokuapp.com/', githubRepo: 'https://github.com/Ren3546/Note_Taker' },
+    { title: 'Note Taker', imageUrl: noteApp, link: 'https://note-app-rv-b9e70abbcc56.herokuapp.com/', githubRepo: 'https://github.com/Ren3546/Note_Taker' },
     { title: 'Dog Breed Quiz', imageUrl: dog, link: 'https://ren3546.github.io/Dog-Breed-Quiz/', githubRepo: 'https://github.com/Ren3546/Dog-Breed-Quiz' },
     { title: 'Employee Tracker', imageUrl: employee, link: 'https://drive.google.com/file/d/1eKjFAitLmzU0ehniZNfEizqsiCYuTuYK/view', githubRepo: 'https://github.com/Ren3546/Employee_Tracker' },
     { title: 'Social Network Api', imageUrl: social, link: ' https://drive.google.com/file/d/1iGmAFGIzRH4k_SJJXIZk0m5jOlkrkaNt/view?usp=sharing', githubRepo: 'https://github.com/Ren3546/Social_Network_Api' },
