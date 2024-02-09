835a5e10c14ff36fe0e363b3e99b7747ef42e00b
Ren3546 <128203284+Ren3546@users.noreply.github.com>
2024-02-08 21:50:59 -0500

    updated image import error

--- src/components/aboutme.jsx
+++ src/components/aboutme.jsx
@@ -1,8 +1,10 @@
+import profile from '../assets/profile.jpg'
+
 export default function Aboutme() {
     return (
         <section id="about" className="bg-gray-200 py-20">
             <div className="container mx-auto px-4 flex flex-col items-center">
-                <img src="./src/assets/profile.jpg" alt="AboutMe Picture" className="h-60 w-auto rounded-full" />
+                <img src={profile} alt="AboutMe Picture" className="h-60 w-auto rounded-full" />
                 <p className="text-center text-xl text-gray-800 mt-12">Welcome to my portfolio! My name is Renato Valdez and I am a Registered Veterinary Technician. I got my diploma from Seneca College. My hobbies include playing video games, art hobbies such as making patches and shadowboxes, and playing with my many pets. I learned full stack web development to hopefully get involved with web development for the veterinary industry. Seeing as the industry is becoming more and more online, I found it would be a great opportunity to apply my knowledge in the field into tools that can improve the industry.   </p>
             </div>
         </section>
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
+import Etsy from '../assets/etsy.png';
+import Linkedin from '../assets/linkedin.png';
+import Github from '../assets/github.png';
 
 const Footer = () => {
   return (
     <div className="Footer flex justify-center items-center bg-gray-500 h-20">
       {/* Etsy */}
       <a href="https://www.etsy.com/ca/shop/RetroFramePerfect" target="_blank" rel="noopener noreferrer">
-        <img src="./src/assets/etsy.png" alt="Etsy" className="mr-4 cursor-pointer h-10" />
+        <img src= {Etsy} alt="Etsy" className="mr-4 cursor-pointer h-10" />
       </a>
       {/* LinkedIn */}
       <a href="https://www.linkedin.com/in/renato-v-b267892b3/" target="_blank" rel="noopener noreferrer">
-        <img src="./src/assets/linkedin.png" alt="LinkedIn" className="mr-4 cursor-pointer h-10" />
+        <img src={Linkedin} alt="LinkedIn" className="mr-4 cursor-pointer h-10" />
       </a>
       {/* GitHub */}
       <a href="https://github.com/Ren3546" target="_blank" rel="noopener noreferrer">
-        <img src="./src/assets/github.png" alt="GitHub" className="cursor-pointer h-16" />
+        <img src={Github} alt="GitHub" className="cursor-pointer h-16" />
       </a>
     </div>
   );
